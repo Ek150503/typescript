813836fc60f2299df5772cd9a3e56d0433d7ed24
Ek Ae <ekzin15@gmail.com>
2023-01-20 14:47:31 +0700

    Instance Method Modifiers

--- features/classes.ts
+++ features/classes.ts
@@ -1,22 +1,27 @@
 class Vehicle {
-  drive(): void {
-    console.log('Chugga chuuggga');
-  }
+  // public drive(): void {
+  //   console.log('Chugga chuuggga');
+  // }
 
-  honk(): void {
+  protected honk(): void {
     console.log('Hunk');
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log('Vlommmm!');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+// vehicle.drive();
+// vehicle.honk();
 
 const car = new Car();
-car.drive();
+car.startDrivingProcess();
