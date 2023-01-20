e9916115bfda27c5ab0e41f99942ac5b820c7132
Ek Ae <ekzin15@gmail.com>
2023-01-20 12:27:37 +0700

    more on func type

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -2,6 +2,15 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 
-const add1: (a: number, b: number) => number = (a, b) => {
+type func = (a: number, b: number) => number;
+
+// const add1: (a: number, b: number) => number = (a, b) => {
+//   return a + b;
+// };
+const add1: func = (a, b) => {
   return a + b;
 };
+
+interface Props {
+  onclick: () => void;
+}
