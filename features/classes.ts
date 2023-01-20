e2880dec1e5d073b1d47ae04ff829af5287158ad
Ek Ae <ekzin15@gmail.com>
2023-01-20 14:36:15 +0700

    Basic Inheritance

--- features/classes.ts
+++ features/classes.ts
@@ -8,6 +8,15 @@ class Vehicle {
   }
 }
 
+class Car extends Vehicle {
+  drive(): void {
+    console.log('Vlommmm!');
+  }
+}
+
 const vehicle = new Vehicle();
 vehicle.drive();
 vehicle.honk();
+
+const car = new Car();
+car.drive();
