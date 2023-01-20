b984a247c90a539672413191967c79fd0d2077a0
Ek Ae <ekzin15@gmail.com>
2023-01-20 14:04:55 +0700

    Fixing Long Annotations with Interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,14 +1,16 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: 'Civic',
   year: 2000,
   broken: true,
 };
 
-const printVihicle = (vihicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
+const printVihicle = (vihicle: Vehicle): void => {
   console.log(`
   the vihicle ${vihicle.name} has been released since ${vihicle.year} and now it's bloken (${vihicle.broken})
   `);
