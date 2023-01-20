36244c15aeff05bddd38d0aa81bf7813480e4b31
Ek Ae <ekzin15@gmail.com>
2023-01-20 14:55:16 +0700

    Fields in Classes

--- features/classes.ts
+++ features/classes.ts
@@ -3,12 +3,17 @@ class Vehicle {
   //   console.log('Chugga chuuggga');
   // }
 
+  constructor(public color: string) {}
   protected honk(): void {
     console.log('Hunk');
   }
 }
 
 class Car extends Vehicle {
+  constructor(color: string) {
+    super(color);
+  }
+
   private drive(): void {
     console.log('Vlommmm!');
   }
@@ -19,9 +24,10 @@ class Car extends Vehicle {
   }
 }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle('Black');
+console.log(vehicle.color);
 // vehicle.drive();
 // vehicle.honk();
 
-const car = new Car();
+const car = new Car('Yellow');
 car.startDrivingProcess();
