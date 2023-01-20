98b582e43f31b6e1240d7849c3b8c66eb99107a7
Ek Ae <ekzin15@gmail.com>
2023-01-20 12:46:21 +0700

    Void and Never

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -30,3 +30,21 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError3 = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) throw new Error(message);
+};
+
+const throwError1 = (message: string): string => {
+  if (!message) throw new Error(message);
+
+  return message;
+};
