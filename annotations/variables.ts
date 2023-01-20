6d496293dac27028cbdc50effd3f7a082f14df2d
Ek Ae <ekzin15@gmail.com>
2023-01-20 11:15:00 +0700

    Annotations with Variables

---
+++ annotations/variables.ts
@@ -0,0 +1,9 @@
+const apples: number = 5;
+const speed: string = 'fast';
+const hasname: boolean = false;
+
+const nothingMuch: null = null;
+const nothing: undefined = undefined;
+
+// buitin objects
+const now: Date = new Date();
