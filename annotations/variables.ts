21648be15c2c7a635a206203ab808133616467fb
Ek Ae <ekzin15@gmail.com>
2023-01-20 11:25:26 +0700

    Object Literal Annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -7,3 +7,22 @@ const nothing: undefined = undefined;
 
 // buitin objects
 const now: Date = new Date();
+
+//array
+let colors: string[] = ['red', 'green', 'yellow'];
+let myNumber: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, false];
+
+//class
+class Car {}
+
+let car: Car = new Car();
+
+// objects literal
+let point: {
+  x: number;
+  y: string;
+} = {
+  x: 10,
+  y: 'name',
+};
