831e987d854d420b3c85a2d6d8a1d14ecd364320
Ek Ae <ekzin15@gmail.com>
2023-01-20 13:15:49 +0700

    Arrays in Typescript

---
+++ features/arrays.ts
@@ -0,0 +1,9 @@
+const carMaker: string[] = ['toyota', 'chevy', 'ford'];
+
+const a: number[] = [];
+
+const dates = [new Date(), new Date()];
+
+const carByMake = [['f150'], ['corola'], ['chevy']];
+
+const carByMake1: string[][] = [];
